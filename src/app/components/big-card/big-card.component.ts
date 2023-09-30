d93e9236e7ca1ea3c7f94f3c6661c4ddf2c1c4f7
Tallison Tadeu de Jesus <tallisontadeu_037@hotmail.com>
2023-09-30 17:23:00 -0300

    news cards

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  photoCover:string=""
+  @Input()
+  cardTitle:string=""
+  @Input()
+  cardDescrition:string=""
+  constructor() {
+
+   }
 
   ngOnInit(): void {
   }
